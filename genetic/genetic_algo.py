cd232f6e798974d7d4c44f11d313378627174ab3
Claudio Carvalho <cco2@cin.ufpe.br>
2021-03-23 23:07:15 -0300

    refactor: gui

--- genetic/genetic_algo.py
+++ genetic/genetic_algo.py
@@ -8,8 +8,8 @@ class DNA:
     def __init__(self, G):
         self.genes = create_random_vector(G)
 
-    def crossover(self, partner):
-        child = copy.deepcopy(self)
+    def crossover(self, partner, G):
+        child = DNA(G)
         for i in range(len(self.genes[0])):
             if random.random() >= 0.5:
                 child.genes[0][i] = partner.genes[0][i]
@@ -18,26 +18,31 @@ class DNA:
         return child
 
     def mutate(self, mutation_rate):
-        for i in range(len(self.genes)):
-            if random.random() < mutation_rate:
+        for i in range(len(self.genes[0])):
+            mutation = random.random()
+            if mutation < mutation_rate:
                 self.genes[0][i] = np.random.RandomState().randn()
 
-def generate_initial_population(quant, G):
-    return [DNA(G) for _ in range(quant)]
+class Population:
+    def __init__(self, G, population_size):
+        self.G = G
+        self.individuals = [DNA(G) for _ in range(population_size)]
 
-def _build_mating_pool(population, fitness):
-    mating_pool = []
-    for i in range(len(population)):
-        mating_pool += [population[i]] * fitness[i]
-    return mating_pool
+    def _build_mating_pool(self, fitness):
+        mating_pool = []
+        for i in range(len(self.individuals)):
+            mating_pool += [self.individuals[i]] * fitness[i]
+        if len(mating_pool) <= 0:
+            mating_pool = [DNA(self.G) for _ in range(len(self.individuals))]
+        return mating_pool
 
-def evolve(population, fitness, mutation_rate):
-    mating_pool = _build_mating_pool(population, fitness)
-    new_population = []
-    for _ in range(len(population)):
-        mother = random.choice(population)
-        father = random.choice(population)
-        child = mother.crossover(father)
-        child.mutate(mutation_rate)
-        new_population.append(child)
-    return new_population
+    def evolve(self, fitness, mutation_rate):
+        mating_pool = self._build_mating_pool(fitness)
+        new_individuals = []
+        for _ in range(len(self.individuals)):
+            mother = random.choice(mating_pool)
+            father = random.choice(mating_pool)
+            child = mother.crossover(father, self.G)
+            child.mutate(mutation_rate)
+            new_individuals.append(child)
+        self.individuals = new_individuals
--- genetic/gui.py
+++ genetic/gui.py
@@ -1,69 +1,86 @@
-from tkinter import *
+import tkinter as tk
 from PIL import ImageTk, Image
 from . import genetic_algo
 from genetic.generate_from_latent import generate_image
 
+class LatentHiker(tk.Frame):
+    def __init__(self, parent,population_size, G, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.configure(background='black')
 
-def start_gui(population_size, G):
-    window = Tk()
-    window.resizable(width=False, height=False)
-    window.title('Latent Hiker')
-    window.configure(background='black')
+        photo1 = ImageTk.PhotoImage(Image.open("./genetic/gato_pizza.jpg").resize((256, 256)))
+        tk.Label(self, bg='black', fg='black').grid(row=0, column=0, pady=(5,0))
 
-    photo1 = ImageTk.PhotoImage(Image.open("./genetic/gato_pizza.jpg").resize((256, 256)))
-    Label(window, bg='black', fg='black').grid(row=0, column=0, pady=(5,0))
+        self.G = G
+        self.population = genetic_algo.Population(self.G, population_size)
 
-    population = genetic_algo.generate_initial_population(population_size, G)
+        self.images = []
+        self.sliders = []
+        self.generated_images_cache = []
+        row = -1
+        screen_width = self.winfo_screenwidth()
+        cells_per_row = screen_width // 280
+        for i, individual in enumerate(self.population.individuals):
+            if i % (cells_per_row+1) == 0:
+                row += 2
+            col = (i % (cells_per_row+1))
 
-    images = []
-    sliders = []
-    generated_images_cache = []
-    row = -1
-    for i in range(population_size):
-        if i % 5 == 0:
-            row += 2
-        col = (i % 5)
+            generated_image = ImageTk.PhotoImage(generate_image(G, individual.genes).resize((256, 256)))
+            self.generated_images_cache.append(generated_image)
 
-        generated_image = ImageTk.PhotoImage(generate_image(G, population[i].genes).resize((256, 256)))
-        generated_images_cache.append(generated_image)
+            image = tk.Label(self, image=generated_image, bg='black')
+            image.grid(row=row, column=col, padx=(5, 5))
+            self.images.append(image)
+            slider = tk.Scale(self, from_=0, to=10, orient=tk.HORIZONTAL, bg='black', fg='white')
+            slider.grid(row=row+1, column=col, pady=(5, 25))
+            self.sliders.append(slider)
+        row += 2            
 
-        image = Label(window, image=generated_image, bg='black')
-        image.grid(row=row, column=col, padx=(5, 5))
-        images.append(image)
-        slider = Scale(window, from_=0, to=10, orient=HORIZONTAL, bg='black', fg='white')
-        slider.grid(row=row+1, column=col, pady=(5, 25))
-        sliders.append(slider)
-    row += 2
+        tk.Label(self, text='Mutation Rate:', bg='black', fg='white').grid(row=row, column=0)
+        row += 1
+        self.mutation_input_box = tk.Entry(self)
+        self.mutation_input_box.insert(tk.END, '0.01')
+        self.mutation_input_box.grid(row=row, column=0)
+        tk.Button(self, text='UPDATE', width=16, bg='white', fg='black', command=self._update).grid(row=row, column=cells_per_row)
+        row += 1
 
-    def update():
-        nonlocal images
-        nonlocal sliders
+        tk.Label(self, bg='black', fg='black').grid(row=row, column=0, pady=(5,0))
+
+    def _update(self):
         fitness = []
-        for slider in sliders:
+        for slider in self.sliders:
             fitness.append(slider.get())
             slider.configure(state='disabled')
 
-        nonlocal population
-        nonlocal mutation_input_box
-        population = genetic_algo.evolve(population, fitness, float(mutation_input_box.get()))
+        self.population.evolve(fitness, float(self.mutation_input_box.get()))
 
-        for i in range(population_size):
-            generated_image = ImageTk.PhotoImage(generate_image(G, population[i].genes).resize((256, 256)))
-            generated_images_cache[i] = generated_image
+        for i in range(len(self.population.individuals)):
+            generated_image = ImageTk.PhotoImage(generate_image(self.G, self.population.individuals[i].genes).resize((256, 256)))
+            self.generated_images_cache[i] = generated_image
 
-            images[i].configure(image=generated_image)
-            sliders[i].configure(state='normal')
-            sliders[i].set(0)
-            
-        
-    Label(window, text='Mutation Rate:', bg='black', fg='white').grid(row=row, column=0)
-    row += 1
-    mutation_input_box = Entry()
-    mutation_input_box.insert(END, '0.01')
-    mutation_input_box.grid(row=row, column=0)
-    Button(window, text='UPDATE', width=16, bg='white', fg='black', command=update).grid(row=row, column=4)
-    row += 1
+            self.images[i].configure(image=generated_image)
+            self.sliders[i].configure(state='normal')
+            self.sliders[i].set(0)
 
-    Label(window, bg='black', fg='black').grid(row=row, column=0, pady=(5,0))
+def start_gui(population_size, G):
+    root = tk.Tk()
+    root.title('Latent Hiker')
+    root.configure(background='black')
+    root.state('zoomed')
+    # root.resizable(width=False, height=False)
 
-    window.mainloop()
+    main_frame = tk.Frame(root, bg='black')
+    main_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    canvas = tk.Canvas(main_frame)
+    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    scrollbar = tk.Scrollbar(main_frame, orient=tk.VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    canvas.configure(yscrollcommand=scrollbar.set, background='black')
+    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox('all')))
+    canvas.bind_all("<MouseWheel>", lambda e: canvas.yview_scroll(-1 * int((e.delta / 60)), "units"))
+    second_frame = tk.Frame(canvas, bg='black')
+    canvas.create_window((0,0), window=second_frame, anchor='nw')
+
+    LatentHiker(second_frame, population_size, G).pack(fill=tk.BOTH, expand=True)
+    root.mainloop()
